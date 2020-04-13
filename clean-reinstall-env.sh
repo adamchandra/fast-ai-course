49263b3f2c020bf30da6e6f43315f375d86c9fda
Adam Saunders <saunders@cs.umass.edu>
2020-04-13 15:58:39 -0400

    ()

--- clean-reinstall-env.sh
+++ clean-reinstall-env.sh
@@ -3,13 +3,11 @@
 SOURCE="BASH_SOURCE[0]"
 SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-ENV=fast-ai-course
+ENV=fastai-course
 
 conda env remove --name $ENV
 conda create --name $ENV --file ./conda-package-list.txt
 
 conda activate $ENV
 echo "using python $(which python)"
-
-## Install openreview-py
 
